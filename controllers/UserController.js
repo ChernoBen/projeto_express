91c44b56b43fe44c07401f02e4477da42dab9542
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-19 09:24:56 -0300

    primeira rota

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,8 +3,24 @@ class UserController{
 
     async create(req,res){
         console.log(req.body)
+        //detruturing
+        var {email,name,password} = req.body;
+        
+        if(email == undefined){
+
+            return res.status(400).json({error:"Email undefined"})
+
+        }else if(name == undefined){
+
+            return res.status(400).json({error:"Name undefined"})
+
+        }else if(password==undefined){
+
+            return res.status(400).json({error:"Password undefined"})
+        }
+
         return res.json({response:req.body})
-        res.send("Pegando corpo da requisição!")
+        //res.send("Pegando corpo da requisição!")
 
     }
 }
