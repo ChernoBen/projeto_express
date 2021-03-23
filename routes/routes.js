60945f0c28bfdd16931af11b60a71ad6c4d102da
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-23 10:29:22 -0300

    configurando cors

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,16 @@
-var express = require("express")
-var app = express();
-var router = express.Router();
-var HomeController = require("../controllers/HomeController");
-var UserController = require("../controllers/UserController");
-var AdminAuth = require('../middleware/AdminAuth');
+const express = require("express")
+const app = express();
+const router = express.Router();
+const HomeController = require("../controllers/HomeController");
+const UserController = require("../controllers/UserController");
+const AdminAuth = require('../middleware/AdminAuth');
+const cors  = require('cors');
+
+app.use(cors());
 
 router.get('/', HomeController.index);
 router.post('/user', UserController.create);
-router.get('/user',AdminAuth, UserController.index);
+router.get('/user', UserController.index);
 router.get('/user/:id',AdminAuth, UserController.findUser);
 router.put('/user',AdminAuth, UserController.edit);
 router.delete('/user/:id',AdminAuth, UserController.remove);
